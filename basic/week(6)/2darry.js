e78dd2f1198ccfd766e5339f09397145aa589eed
Rootsrobut <sumitkumarofficial007@gmail.com>
2025-01-15 11:26:53 +0530

    2-D array

---
+++ basic/week(6)/2darry.js
@@ -0,0 +1,12 @@
+// Create a 2D array (3x3)
+const arr = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+// Print the 2D array
+arr.forEach(row => {
+    console.log(row.join(" "));
+});
+
---
+++ basic/week(6)/2darry.py
@@ -0,0 +1,10 @@
+# Create a 2D array (3x3)
+arr = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+# Print the 2D array
+for row in arr:
+    print(" ".join(map(str, row)))
